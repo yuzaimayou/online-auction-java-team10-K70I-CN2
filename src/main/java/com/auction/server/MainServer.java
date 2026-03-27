e6e40f4f0aa122fb12eb113e3fb8702c1b48430f
khanhvan <25023543@vnu.edu.vn>
2026-03-27 19:53:45 +0700

    fix: restore MainServer from main branch

--- src/main/java/com/auction/server/MainServer.java
+++ src/main/java/com/auction/server/MainServer.java
@@ -2,13 +2,35 @@ package com.auction.server;
 
 import com.auction.server.controller.ClientHandler;
 import com.auction.server.service.AuthService;
+import com.auction.server.database.DatabaseInit;
+
+import java.io.IOException;
 import java.net.Socket;
+import java.net.ServerSocket;
 
 public class MainServer {
-        AuthService authService = new AuthService();
-            while (true) {
+    private static final int PORT=8000;
 
+    public static void main(String[] agrs){
+        // tao database
+        DatabaseInit.init();
+        //Khoi tao cac service cot loi
+        AuthService authService=new AuthService();
+        authService.register("admin", "admin");
+        try (ServerSocket serverSocket=new ServerSocket(PORT)){
+            System.out.println("Port has opened!");
+            while(true){
+                Socket clientSocket=serverSocket.accept();
+                System.out.printf("Client has connected with IP: %s%n",clientSocket.getInetAddress().getHostAddress());
+
+                ClientHandler handler=new ClientHandler(clientSocket,authService);
+                Thread clientThread= new Thread(handler);
+
+                clientThread.start();
             }
+        }catch (IOException e){
+            System.err.println(e.getMessage());
         }
+
     }
 }
