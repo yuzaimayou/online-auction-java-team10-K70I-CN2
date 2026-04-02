2b45ca441b6d863b738a1b35871d4ba97c9d207a
yuzaimayou <ducanhng0401@gmail.com>
2026-04-02 14:19:53 +0700

    fixed client-server

--- src/main/java/com/auction/client/controller/RegisterController.java
+++ src/main/java/com/auction/client/controller/RegisterController.java
@@ -8,6 +8,7 @@ import com.auction.shared.model.AuthPayload;
 import com.google.gson.Gson;
 
 import javafx.animation.PauseTransition;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,6 +22,7 @@ import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
 public class RegisterController {
 
@@ -39,7 +41,7 @@ public class RegisterController {
     @FXML
     private Label lblMessage;
 
-    private NetworkService network = NetworkService.getInstance();
+    private NetworkService network=NetworkService.getInstance();
     private Gson gson = new Gson();
 
     @FXML
@@ -63,30 +65,41 @@ public class RegisterController {
 
         AuthPayload payload = new AuthPayload(username, password);
         String jsonPayload = gson.toJson(payload);
+        CompletableFuture.supplyAsync(()->{
+            return network.sendRequest(new RequestMessage(ActionType.REGISTER,jsonPayload));
+        }).thenAccept(res->{
+            if (res == null) {
+                Platform.runLater(()->{
+                    lblMessage.setTextFill(Color.RED);
+                    lblMessage.setText("Không thể kết nối server");
+                });
 
-        ResponseMessage res =
-                network.sendRequest(new RequestMessage(ActionType.REGISTER, jsonPayload));
+                return;
+            }
 
-        if (res == null) {
-            lblMessage.setTextFill(Color.RED);
-            lblMessage.setText("Không thể kết nối server");
-            return;
-        }
+            if ("SUCCESS".equals(res.getStatus())) {
+                System.out.println(res.getMessage());
+                Platform.runLater(()->{
+                    lblMessage.setTextFill(Color.GREEN);
+                    lblMessage.setText("Đăng ký thành công");
+                    PauseTransition pause = new PauseTransition(Duration.seconds(2));
+                    pause.setOnFinished(e -> handleSwitchToLogin(event));
+                    pause.play();
+                });
 
-        if ("SUCCESS".equals(res.getStatus())) {
 
-            lblMessage.setTextFill(Color.GREEN);
-            lblMessage.setText("Đăng ký thành công");
-            PauseTransition pause = new PauseTransition(Duration.seconds(5));
-            pause.setOnFinished(e -> handleSwitchToLogin(event));
-            pause.play();
+            } else {
+                Platform.runLater(()->{
 
-        } else {
+                    lblMessage.setTextFill(Color.RED);
+                    lblMessage.setText(res.getMessage());
+                });
 
-            lblMessage.setTextFill(Color.RED);
-            lblMessage.setText(res.getMessage());
 
-        }
+            }
+        });
+
+
     }
 
     @FXML
--- src/main/java/com/auction/server/controller/ClientHandler.java
+++ src/main/java/com/auction/server/controller/ClientHandler.java
@@ -4,8 +4,8 @@ import com.auction.server.service.AuthService;
 import com.auction.shared.message.RequestMessage;
 import com.auction.shared.message.ResponseMessage;
 import com.auction.shared.model.AuthPayload;
-import com.google.gson.Gson;
 import com.auction.shared.model.account.User;
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,33 +16,37 @@ import java.net.Socket;
 public class ClientHandler implements Runnable {
     private Socket clientSocket;
     private AuthService authService;
-    private Gson gson= new Gson();
+    private Gson gson = new Gson();
 
-    public ClientHandler(Socket socket, AuthService authService){
-        this.clientSocket=socket;
-        this.authService=authService;
+    public ClientHandler(Socket socket, AuthService authService) {
+        this.clientSocket = socket;
+        this.authService = authService;
     }
-    private void loginAction(String payload,ResponseMessage response){
-        AuthPayload authData=gson.fromJson(payload,AuthPayload.class);
+
+    private boolean loginAction(String payload, ResponseMessage response) {
+        AuthPayload authData = gson.fromJson(payload, AuthPayload.class);
         String username = authData.getUsername();
         String password = authData.getPassword();
-        System.out.println(username+','+password);
+        System.out.println(username + ',' + password);
 
-        User loggedInuser=authService.login(username,password);
-        if(loggedInuser!=null){
+        User loggedInuser = authService.login(username, password);
+        if (loggedInuser != null) {
             response.setStatus("SUCCESS");
             response.setMessage("Log in successful");
             response.setData(gson.toJson(loggedInuser));
             System.out.println("success");
-        } else{
+            return true;
+        } else {
             System.out.println("Incorrect");
             response.setStatus("FAIL");
             response.setMessage("Incorrect username or password");
+            return false;
         }
 
     }
-    private void registerAction(String payload, ResponseMessage response) {
-        AuthPayload authData=gson.fromJson(payload,AuthPayload.class);
+
+    private boolean registerAction(String payload, ResponseMessage response) {
+        AuthPayload authData = gson.fromJson(payload, AuthPayload.class);
         String username = authData.getUsername();
         String password = authData.getPassword();
 
@@ -56,31 +60,55 @@ public class ClientHandler implements Runnable {
             response.setStatus("FAIL");
             response.setMessage("Username already exists");
         }
+        return false;
     }
+
     @Override
-    public void run(){
-        try(BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true)
-        ){
+    public void run() {
+        try {
+            clientSocket.setSoTimeout(15000);
+        } catch (Exception e) {
+            System.err.println("Loi set timeout");
+        }
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+        ) {
             String jsonRequest;
-            while ((jsonRequest=in.readLine()) !=null){
-                RequestMessage request =gson.fromJson(jsonRequest,RequestMessage.class);
-                ResponseMessage response= new ResponseMessage();
-                switch (request.getAction()){
-                    case LOGIN -> loginAction(request.getPayload(),response);
-                    case REGISTER -> registerAction(request.getPayload(), response);
+            while ((jsonRequest = in.readLine()) != null) {
+                RequestMessage request = gson.fromJson(jsonRequest, RequestMessage.class);
+                ResponseMessage response = new ResponseMessage();
+                boolean keepConnectionAlive = true;
+                System.out.println("Kiem tra action");
+                switch (request.getAction()) {
+                    case LOGIN -> keepConnectionAlive = loginAction(request.getPayload(), response);
+                    case REGISTER -> keepConnectionAlive = registerAction(request.getPayload(), response);
                     default -> {
                         response.setStatus("ERROR");
                         response.setMessage("Invalid action");
+                        keepConnectionAlive = false;
                     }
                 }
-                String jsonResponse=gson.toJson(response);
+                String jsonResponse = gson.toJson(response);
+
                 out.println(jsonResponse);
-                System.out.println("Server was sent: "+jsonResponse);
+                System.out.println("Server was sent: " + jsonResponse);
+                if (!keepConnectionAlive) {
+                    System.out.println("Ngắt kết nối với client.");
+
+                    break;
+                } else {
+                    clientSocket.setSoTimeout(0);
+                }
             }
 
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Failed to connect to server: " + e.getMessage());
+        } finally {
+            try {
+                if (!clientSocket.isClosed()) clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
