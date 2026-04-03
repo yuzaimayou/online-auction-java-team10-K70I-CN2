3bdeacfbbd7f345cd889418d755ad9e1277e1e77
yuzaimayou <ducanhng0401@gmail.com>
2026-04-03 14:22:11 +0700

    temp

---
+++ src/main/java/com/auction/client/controller/AddAuction.java
@@ -0,0 +1,4 @@
+package com.auction.client.controller;
+
+public class AddAuction {
+}
--- src/main/java/com/auction/client/controller/HomePageController.java
+++ src/main/java/com/auction/client/controller/HomePageController.java
@@ -1,40 +1,51 @@
 package com.auction.client.controller;
 
-import com.auction.client.service.NetworkService;
-import com.auction.shared.constant.ActionType;
-import com.auction.shared.message.RequestMessage;
-import com.auction.shared.message.ResponseMessage;
-
-import com.auction.shared.model.AuthPayload;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import com.google.gson.Gson;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HomePageController {
     @FXML
     private Label labelUsername;
+    @FXML
+    private StackPane formAddAuction;
 
-    public HomePageController(){};
+    public HomePageController() {
+    }
 
     @FXML
     public void initialize() {
         System.out.println("Đã vào trang chủ!");
     }
-    public void initData(String userName,String role){
-        if(userName!=null){
+
+    @FXML
+    public void addAuction() {
+        try {
+            System.out.println("Tao phien dau gia moi");
+            Parent formRoot = FXMLLoader.load(getClass().getResource("/com.auction.client/fxml/FormAddAuction.fxml"));
+            if (formAddAuction != null) {
+                formAddAuction.getChildren().clear();
+                formAddAuction.getChildren().add(formRoot);
+            } else {
+                System.err.println("Error: Khong tim thay StackPane");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed");
+        }
+
+
+    }
+
+    public void initData(String userName, String role) {
+        if (userName != null) {
             labelUsername.setText(userName);
+
         }
     }
 }
