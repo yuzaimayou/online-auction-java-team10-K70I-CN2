1c38c1627a59b5f28ca0ef82d10cff4d7130ab69
yuzaimayou <ducanhng0401@gmail.com>
2026-04-04 14:41:28 +0700

    test

--- src/main/java/com/auction/shared/message/RequestMessage.java
+++ src/main/java/com/auction/shared/message/RequestMessage.java
@@ -5,17 +5,30 @@ import com.auction.shared.constant.ActionType;
 public class RequestMessage {
     private String payload;
     private ActionType action;
-    public RequestMessage(){}
-    public RequestMessage(ActionType action, String payload){
-        this.action=action;
-        this.payload=payload;
-    }
-    public ActionType getAction(){return action;}
-    public void setAction(ActionType action){this.action=action;}
-    public String getPayload(){return payload;}
-    public void setPayload(String payload){this.payload=payload;}
 
-    public ActionType getActionType() {
-        return  action;
+    public RequestMessage() {
     }
+
+    public RequestMessage(ActionType action, String payload) {
+        this.action = action;
+        this.payload = payload;
+    }
+
+    public ActionType getAction() {
+        return action;
+    }
+
+    public void setAction(ActionType action) {
+        this.action = action;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+`
 }
--- src/main/java/com/auction/shared/message/ResponseMessage.java
+++ src/main/java/com/auction/shared/message/ResponseMessage.java
@@ -1,22 +1,39 @@
 package com.auction.shared.message;
 
 public class ResponseMessage {
-    private String status, message,data;
-    public ResponseMessage(){}
+    private String status, message, data;
 
-    public ResponseMessage(String status,String message, String data){
-        this.status=status;
-        this.message=message;
-        this.data=data;
+    public ResponseMessage() {
     }
 
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
+    public ResponseMessage(String status, String message, String data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
 
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
+    public String getStatus() {
+        return status;
+    }
 
-    public String getData() { return data; }
-    public void setData(String data) { this.data = data; }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 
 }
