10f57df36696c795ff7ecd73486e09dc6dd4f5a5
yuzaimayou <ducanhng0401@gmail.com>
2026-04-04 14:41:28 +0700

    test

--- src/main/java/com/auction/client/controller/HomePageController.java
+++ src/main/java/com/auction/client/controller/HomePageController.java
@@ -1,13 +1,5 @@
 package com.auction.client.controller;
 
-import com.auction.client.service.NetworkService;
-import com.auction.shared.constant.ActionType;
-import com.auction.shared.message.RequestMessage;
-import com.auction.shared.message.ResponseMessage;
-import com.auction.shared.model.AuthPayload;
-import com.google.gson.Gson;
-
-import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,20 +7,23 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class HomePageController {
     @FXML
+    public Label userName;
+
+    @FXML
     public void initialize() {
         System.out.println("Đã vào trang chủ!");
+    }
+
+    @FXML
+    public void initData(String username, String role) {
+        userName.setText(username);
     }
 
     @FXML
