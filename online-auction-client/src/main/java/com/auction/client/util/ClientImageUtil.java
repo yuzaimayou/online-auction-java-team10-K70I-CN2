5f9db9354146d25f728b687bee828d2da8540816
yuzaimayou <ducanhng0401@gmail.com>
2026-04-18 16:28:41 +0700

    appconfig

--- online-auction-client/src/main/java/com/auction/client/util/ClientImageUtil.java
+++ online-auction-client/src/main/java/com/auction/client/util/ClientImageUtil.java
@@ -3,9 +3,11 @@ package com.auction.client.util;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import static com.auction.client.util.AppConfig.getImageUrl;
+
 public class ClientImageUtil {
     public static void displayImage(String imageName, String source, ImageView imageId) {
-        String imageUrl = String.format("http://localhost:1401/%s/%s", source, imageName);
+        String imageUrl = String.format("%s/%s/%s", getImageUrl(), source, imageName);
         Image fxImage = new Image(imageUrl, true);
 
         imageId.setImage(fxImage);
