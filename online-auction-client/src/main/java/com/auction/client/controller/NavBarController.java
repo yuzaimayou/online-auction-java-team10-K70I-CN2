781110841d3a17d599edcec326d4d65477600151
khanhvan <25023543@vnu.edu.vn>
2026-05-04 18:15:53 +0700

    Viết chức năng cho thanh search bar

--- online-auction-client/src/main/java/com/auction/client/controller/NavBarController.java
+++ online-auction-client/src/main/java/com/auction/client/controller/NavBarController.java
@@ -4,19 +4,24 @@ import com.auction.client.util.NavigationUtil;
 import com.auction.client.util.UserSession;
 import com.auction.shared.model.account.User;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
 public class NavBarController {
     private User user = UserSession.getInstance().getLoggedInUser();
     @FXML
     private Label userName;
+    @FXML
+    private TextField searchField;
 
     public void initialize() {
         userName.setText(user.getUsername());
 
+        searchField.textProperty().bindBidirectional(SearchStoreController.searchQueryProperty());
     }
-
     public void handleSwitchToHome() {
+        SearchStoreController.reset();
         NavigationUtil.handleSwitchToHomePage(userName);
     }
 
