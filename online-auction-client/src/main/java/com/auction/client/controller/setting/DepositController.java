3de689c3a9c18c6c1371599c61e309671cc8fc0f
khanhvan <25023543@vnu.edu.vn>
2026-05-19 06:48:46 +0700

    update hiển thị số tiền

--- online-auction-client/src/main/java/com/auction/client/controller/setting/DepositController.java
+++ online-auction-client/src/main/java/com/auction/client/controller/setting/DepositController.java
@@ -1,5 +1,10 @@
 package com.auction.client.controller.setting;
 
+import com.auction.client.service.DepositService;
+import com.auction.client.util.UserSession;
+import com.auction.client.service.ToastService;
+import com.auction.shared.model.account.User;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -7,7 +12,6 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 
 public class DepositController {
-
     @FXML
     private TextField amountField;
     @FXML
@@ -35,12 +39,11 @@ public class DepositController {
         if (amountGroup.getSelectedToggle() == null) {
             amountField.clear();
         } else {
-            String amount = clickedButton.getText();
+            String amount = clickedButton.getText().replace("$", "").replace(",", "").trim();
             amountField.setText(amount);
         }
         isUpdatingFromButton = false;
     }
-
 
     @FXML
     private void handleBackToWallet(ActionEvent event) {
@@ -51,7 +54,37 @@ public class DepositController {
 
     @FXML
     private void handleConfirmDeposit(ActionEvent event) {
-        String amount = amountField.getText();
-        System.out.println("Đang xử lý nạp tiền: " + amount);
+        String amountText = amountField.getText().trim();
+        if (amountText.isEmpty()) {
+            ToastService.showError(amountField.getScene(), "Vui lòng nhập số tiền!");
+            return;
+        }
+
+        double amount;
+        try {
+            amount = Double.parseDouble(amountText);
+        } catch (NumberFormatException e) {
+            ToastService.showError(amountField.getScene(), "Số tiền không hợp lệ!");
+            return;
+        }
+
+        User currentUser = UserSession.getInstance().getLoggedInUser();
+        if (currentUser == null) return;
+
+        DepositService.getInstance().deposit(currentUser.getId(), amount)
+                .thenAccept(newBalance -> {
+                    Platform.runLater(() -> {
+                        currentUser.setBalance(newBalance);
+
+                        ToastService.showSuccess(amountField.getScene(), "Nạp tiền thành công!");
+                        handleBackToWallet(null);
+                    });
+                })
+                .exceptionally(ex -> {
+                    Platform.runLater(() -> {
+                        ToastService.showError(amountField.getScene(), ex.getCause().getMessage());
+                    });
+                    return null;
+                });
     }
 }
