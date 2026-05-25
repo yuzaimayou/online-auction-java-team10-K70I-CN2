f33e7688a1b6dec0b6cb5adf9e29a4e756e07970
khanhvan <25023543@vnu.edu.vn>
2026-05-26 05:53:23 +0700

    update Ui

--- online-auction-client/src/main/java/com/auction/client/util/AlertUtil.java
+++ online-auction-client/src/main/java/com/auction/client/util/AlertUtil.java
@@ -2,11 +2,9 @@ package com.auction.client.util;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
+import javafx.stage.Window;
 
-/**
- * Util hiển thị Alert dùng chung toàn client.
- * Mỗi controller không nên tự dựng Alert riêng.
- */
 public class AlertUtil {
 
     private AlertUtil() {}
@@ -18,10 +16,14 @@ public class AlertUtil {
     public static void showInfo(String title, String message) {
         show(Alert.AlertType.INFORMATION, title, message);
     }
+
     public static boolean showConfirm(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, message, ButtonType.YES, ButtonType.NO);
+        configure(alert);
+
         alert.setTitle(title);
         alert.setHeaderText(null);
+
         return alert.showAndWait()
                 .filter(r -> r == ButtonType.YES)
                 .isPresent();
@@ -29,9 +31,32 @@ public class AlertUtil {
 
     private static void show(Alert.AlertType type, String title, String message) {
         Alert alert = new Alert(type);
+        configure(alert);
+
         alert.setTitle(title);
         alert.setHeaderText(null);
         alert.setContentText(message);
+
         alert.showAndWait();
+    }
+
+    private static void configure(Alert alert) {
+
+        alert.initModality(Modality.APPLICATION_MODAL);
+        Window window = Window.getWindows()
+                .stream()
+                .filter(Window::isShowing)
+                .findFirst()
+                .orElse(null);
+
+        if (window != null) {
+            alert.initOwner(window);
+        }
+        alert.setResizable(false);
+
+        // 4. size hợp lý
+        alert.getDialogPane().setPrefWidth(360);
+        alert.getDialogPane().setMaxWidth(420);
+        alert.getDialogPane().setPrefHeight(180);
     }
 }
